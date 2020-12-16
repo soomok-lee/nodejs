76bf28e331d9fc7efc2aafabea1bbc5b0f272379
soomok-lee <soomok.us@gmail.com>
2020-12-16 11:15:59 -0800

    POST, ajaxsend.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 // node app.js
 // nodemon app.js
 
+const bodyParser = require('body-parser');
 var express = require('express')
 var app = express()
 app.listen(3000, function() {
@@ -17,8 +18,12 @@ app.listen(3000, function() {
 
 // setting static directory
 app.use(express.static('public'));
+app.use(bodyParser.json()) // json
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs') // npm install ejs --save
 
 // URL routing
+// GET
 app.get('/', function(req, res) {
     // res.send("<h1>hi friend!</h1>")
     res.sendFile(__dirname + "/public/main.html")
@@ -27,4 +32,23 @@ app.get('/', function(req, res) {
 app.get('/main', function(req, res) {
     // res.send("<h1>hi friend!</h1>")
     res.sendFile(__dirname + "/public/main.html")
+});
+
+// POST
+app.post('/email_post', function(req, res) { 
+    // body-parser
+    // npm install body-parser --save
+    // console.log(req.body.email)
+    // res.send("<h1>welcome! " + req.body.email + "</h1>")
+
+    console.log(req.body)
+    // ejs
+    // res.render('email.ejs', {'email' : req.body.email})
+    res.render('email.ejs', req.body)
+});
+
+app.post('/ajax_send_email', function(req, res) {
+    console.log(req.body.email)
+    var responseData = {'result': 'ok', 'email' : req.body.email}
+    res.json(responseData)
 });
