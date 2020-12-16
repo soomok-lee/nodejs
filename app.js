2a4b9c2421f1afe7c895b45e6e3f5b07bf9bd4d1
soomok-lee <soomok.us@gmail.com>
2020-12-16 13:23:18 -0800

    connect to mysql server.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 // node app.js
 // nodemon app.js
 
-const bodyParser = require('body-parser');
 var express = require('express')
 var app = express()
 app.listen(3000, function() {
@@ -18,9 +17,56 @@ app.listen(3000, function() {
 
 // setting static directory
 app.use(express.static('public'));
+// body-parser
+const bodyParser = require('body-parser');
 app.use(bodyParser.json()) // json
 app.use(bodyParser.urlencoded({extended:true}))
+// view engine ejs
 app.set('view engine', 'ejs') // npm install ejs --save
+
+/*
+MySQL 
+C:\Program Files\MySQL\MySQL Server 8.0\bin>mysql.exe -uroot -p<PASSWORD> -h 127.0.0.1 -P 3316 // port 명시
+CREATE DATABASE nodejsDB // DB 생성
+use nodejsDB // DB 지정
+// TABLE 생성
+CREATE TABLE Users(
+    id INT(11) NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(20), 
+    email VARCHAR(200) NOT NULL, 
+    password VARCHAR(200) NOT NULL, 
+    CONSTRAINT Users_PK PRIMARY KEY(id)
+);
+// TABLE 확인
+show tables;
+desc Users;
+// DATA 생성
+INSERT INTO Users(name, email, password) VALUES ('ella', 'ella@ella.com', '1234');
+// DATA 확인 
+SELECT * FROM Users;
+
+// MySQL module
+npm install mysql --save
+
+https://expressjs.com
+
+// connection issue
+https://medium.com/codespace69/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server-consider-8afadc2385e2
+the problem is mysqljs in Node (the package you install with npm install mysql and use it in your Node code) doesn't support the new default authentication method of MySQL 8, yet.
+
+Option 1) Downgrade “MySQL” to authenticate using good old “native_password”
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ella';
+*/
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host:'localhost',
+    port: 3316,
+    user: 'root',
+    password: 'ella',
+    database: 'nodejsdb',
+    insecureAuth : true
+})
+connection.connect()
 
 // URL routing
 // GET
@@ -52,3 +98,4 @@ app.post('/ajax_send_email', function(req, res) {
     var responseData = {'result': 'ok', 'email' : req.body.email}
     res.json(responseData)
 });
+
